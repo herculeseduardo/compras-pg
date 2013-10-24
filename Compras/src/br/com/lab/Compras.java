d2f07c98486df48d3b50f408bfe2638fcdb8f82c
Hercules <hercules.eduardo@gmail.com>
2013-10-23 23:39:24 -0200

    versao inicial

---
+++ Compras/src/br/com/lab/Compras.java
@@ -0,0 +1,14 @@
+package br.com.lab;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class Compras extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
